f9c82de4f42a1d2a9c3570114cee3486c12bdf8a
Anaïs BINET <anais.binet@fms-ea.com>
2026-07-20 15:22:22 +0200

    Ajout de la méthode pour imprimer les informations de l'article dans la console

--- Produc.py
+++ Produc.py
@@ -1,9 +1,16 @@
-class Produc:
+class Product:
     def __init__(self, name, type, stock, price):
         self.name = name
         self.type = type
         self.stock = stock
         self.price = price
+        self.unit = unit
+
+    def __str__(self):
+        print(
+            f"{self.name} : {self.stock} {self.unit}, "
+            f"{self.price:.2f} €/{self.unit}"
+        )
         
     def buy(self, achat):
         self.stock -= achat.stock
