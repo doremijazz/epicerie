c29649c16844f53594e74d8260341f7df28e75a0
Anaïs BINET <anais.binet@fms-ea.com>
2026-07-20 15:07:13 +0200

    Implémentation de la fonction main

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import display
+
+
 def is_in_stock(product, achat):
     return product.stock >= achat.stock
 
@@ -10,6 +13,37 @@ def buy_product(product, achat, client):
 def sell_product(product, achat, client):
     if is_in_stock(product, achat):
         buy_product(product, achat, client)
+
+
+def main(products):
+    clientel = []
+    while True:
+        client = strat_input()
+
+        if client == "stop":
+            display.assessment(clientel)
+            break
+
+        while True:
+            display.catalogue(products)
+            achat = user_input()
+
+            if achat == "stop":
+                display.ticket(client)
+                break
+
+            produit = next(
+                (p for p in products if p.name == achat.name),
+                None
+            )
+
+            sell_product(product, achat, client)
+            clientel.append(client)
+
+
+def strat_input():
+    pass
+
 
 def user_input():
     pass
