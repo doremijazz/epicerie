0cbef7d98576718a9622014d67ee2fa44bf3c79c
Anaïs BINET <anais.binet@fms-ea.com>
2026-07-20 15:24:19 +0200

    Création d'un catalogue de produit test

---
+++ test.py
@@ -0,0 +1,27 @@
+from Produc import Product
+
+products = [
+    # Fruits
+    Product("Clémentine", "Fruit", 6, 2.90, "kg"),
+    Product("Datte", "Fruit", 4, 7.00, "kg"),
+    Product("Grenade", "Fruit", 3, 3.50, "kg"),
+    Product("Kaki", "Fruit", 3, 4.50, "kg"),
+    Product("Kiwi", "Fruit", 5, 3.50, "kg"),
+    Product("Mandarine", "Fruit", 6, 2.80, "kg"),
+    Product("Orange", "Fruit", 8, 1.50, "kg"),
+    Product("Pamplemousse", "Fruit", 8, 2.00, "pièce"),
+    Product("Poire", "Fruit", 5, 2.50, "kg"),
+    Product("Pomme", "Fruit", 8, 1.50, "kg"),
+
+    # Légumes
+    Product("Carotte", "Légume", 7, 1.30, "kg"),
+    Product("Choux de Bruxelles", "Légume", 4, 4.00, "kg"),
+    Product("Chou vert", "Légume", 12, 2.50, "pièce"),
+    Product("Courge butternut", "Légume", 6, 2.50, "pièce"),
+    Product("Endive", "Légume", 5, 2.50, "kg"),
+    Product("Épinard", "Légume", 4, 2.60, "kg"),
+    Product("Poireau", "Légume", 5, 1.20, "kg"),
+    Product("Potiron", "Légume", 6, 2.50, "pièce"),
+    Product("Radis noir", "Légume", 10, 5.00, "pièce"),
+    Product("Salsifis", "Légume", 3, 2.50, "kg"),
+]
