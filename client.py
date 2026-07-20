0ad4ae2586c156d0c8c54e760c8e4306b56c4f18
Anaïs BINET <anais.binet@fms-ea.com>
2026-07-20 15:23:09 +0200

    Ajout de la méthode pour imprimer les informations sur chaque articles acheté

--- client.py
+++ client.py
@@ -3,6 +3,14 @@ class Client:
         self.name = name
         self.surname = surname
         self.ticket = []
+        
+        
+    def __str__(self):
+        for elem in self.ticket:
+            print(
+                f"{elem.name} : {etem.stock} {elem.unit}, "
+                f"{elem.price:.2f} €/{elem.unit}"
+            )
 
     def buy(self, achat, client):
         self.ticket.append(achat)
